d7c645e9b3f296f7e9dc6b9c6751475a62acc871
mohanson <mohanson@outlook.com>
2021-02-07 09:59:43 +0800

    The second wave

--- main.py
+++ main.py
@@ -24,30 +24,30 @@ class Fuzzer:
     def rand_u256(self):
         # There is a higher chance of generating shit numbers
         if random.random() < convention.p_shit_numbers:
-            return hex(random.choice(convention.shit_numbers))
+            return random.choice(convention.shit_numbers)
         else:
-            return hex(random.randint(0, (1 << 64) - 1))
+            return random.randint(0, (1 << 64) - 1)
 
     def rand_idle_register(self):
         return convention.registers[random.randint(0, 30)]
 
     def rand_instruction(self):
         choose_rule = random.choice(convention.instruction_rule)
-        if choose_rule[1] == 'I':
-            opcode = choose_rule[0]
-            rd = self.rand_idle_register()
-            rs = self.rand_idle_register()
-            self.writer.line(f'{opcode} {rd}, {rs}')
-            self.writer.line(f'add t6, t6, {rd}')
-        if choose_rule[1] == 'R':
-            opcode = choose_rule[0]
-            rd = self.rand_idle_register()
-            rs1 = self.rand_idle_register()
-            rs2 = self.rand_idle_register()
-            self.writer.line(f'{opcode} {rd}, {rs1}, {rs2}')
-            self.writer.line(f'add t6, t6, {rd}')
-        if choose_rule[1] == 'R4':
-            pass
+        opcode = choose_rule[0]
+        args = []
+        for i in choose_rule[1]:
+            if i == 'r':
+                args.append(self.rand_idle_register())
+                continue
+            if i == 'u5':
+                args.append(hex(self.rand_u256() % 32))
+                continue
+            if i == 'u6':
+                args.append(hex(self.rand_u256() % 64))
+                continue
+            assert 0
+        self.writer.line(f'{opcode} {", ".join(args)}')
+        self.writer.line(f'add t6, t6, {args[0]}')
 
     def loop(self):
         self.writer.line('.global _start')
@@ -57,26 +57,44 @@ class Fuzzer:
         for _ in range(128):
             # Starts by initializing a registers: x0 to x31
             for i in range(32):
-                self.writer.line(f'li {convention.registers[i]}, {self.rand_u256()}')
+                self.writer.line(f'li {convention.registers[i]}, {hex(self.rand_u256())}')
             for _ in range(1024):
                 self.rand_instruction()
 
         # Returns checksum
+        self.writer.line('')
         self.writer.line('addi a0, t6, 0')
+        self.writer.line('srli t6, t6, 8')
+        self.writer.line('add a0, a0, t6')
+        self.writer.line('srli t6, t6, 16')
+        self.writer.line('add a0, a0, t6')
+        self.writer.line('srli t6, t6, 24')
+        self.writer.line('add a0, a0, t6')
+        self.writer.line('srli t6, t6, 32')
+        self.writer.line('add a0, a0, t6')
+        self.writer.line('srli t6, t6, 40')
+        self.writer.line('add a0, a0, t6')
+        self.writer.line('srli t6, t6, 48')
+        self.writer.line('add a0, a0, t6')
+        self.writer.line('srli t6, t6, 56')
+        self.writer.line('add a0, a0, t6')
         self.writer.line('li a7, 93')
         self.writer.line('ecall')
         self.writer.f.close()
 
 
+done = 0
+
 def main():
     for i in range(1 << 32):
+        if done:
+            break
         print('generation', i)
         f = Fuzzer()
         f.loop()
 
         subprocess.call('/root/app/riscv64b/bin/riscv64-unknown-elf-as -march=rv64gcb -o main.o main.S', shell=True)
         subprocess.call('/root/app/riscv64b/bin/riscv64-unknown-elf-ld -o main main.o', shell=True)
-        subprocess.call('rm main.o', shell=True)
 
         int_output = subprocess.getoutput('/src/ckb-vm-run/target/release/int main')
         int_match = re.match(r'int exit=Ok\((?P<code>-?\d+)\) cycles=\d+', int_output)
@@ -100,4 +118,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        done = 1
--- main.sh
+++ main.sh
@@ -3,6 +3,8 @@ set -x
 /root/app/riscv64b/bin/riscv64-unknown-elf-as -march=rv64gcb -o main.o main.S
 /root/app/riscv64b/bin/riscv64-unknown-elf-ld -o main main.o
 rm main.o
+/src/ckb-vm-run/target/release/int main
 /src/ckb-vm-run/target/release/asm main
+/src/ckb-vm-run/target/release/aot main
 /root/app/riscv64b/bin/spike --isa RV64GCB pk /src/rfuzzing/main
 echo $?
